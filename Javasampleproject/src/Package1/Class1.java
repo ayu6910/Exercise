df143a9ded65cd09e828e46776d1a47ed1cdf59e
ayu6910 <ayuny6910@gmail.com>
2022-05-07 00:39:00 -0400

    I am commit the whole project and push

---
+++ Javasampleproject/src/Package1/Class1.java
@@ -0,0 +1,13 @@
+package Package1;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("some thing coded");
+		System.out.println("some thing coded");
+		System.out.println("some thing coded");
+
+	}
+
+}
