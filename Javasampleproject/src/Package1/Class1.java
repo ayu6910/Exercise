3ff15ade809d633a7288100e5947fc1c47b2a61b
ayu6910 <ayuny6910@gmail.com>
2022-05-08 19:28:29 -0400

    First Push the java project

--- Javasampleproject/src/Package1/Class1.java
+++ Javasampleproject/src/Package1/Class1.java
@@ -4,9 +4,11 @@ public class Class1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("some thing coded");
-		System.out.println("some thing coded");
-		System.out.println("some thing coded");
+		System.out.println("Java first projectcoded");
+		System.out.println("Java first projectcoded");
+		System.out.println("Java first projectcoded");
+		System.out.println("Java first projectcoded");
+		System.out.println("Java first projectcoded");
 
 	}
 
