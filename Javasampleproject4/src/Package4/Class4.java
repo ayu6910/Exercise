0642bbbf415bd1430ab1bbc1222a453da80e6048
ayu6910 <ayuny6910@gmail.com>
2022-05-08 20:52:51 -0400

    This is 4th sample project comment

---
+++ Javasampleproject4/src/Package4/Class4.java
@@ -0,0 +1,16 @@
+package Package4;
+
+public class Class4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is 4th sample project");
+System.out.println("This is 4th sample project");
+System.out.println("This is 4th sample project");
+System.out.println("This is 4th sample project");
+System.out.println("This is 4th sample project");
+System.out.println("This is 4th sample project");
+
+	}
+
+}
