ed5bc14821a26bbaae6fba34908f5f15e53c689e
ayu6910 <ayuny6910@gmail.com>
2022-05-08 20:37:51 -0400

    This is sample project

--- Javasampleproject3/src/package3/Class3.java
+++ Javasampleproject3/src/package3/Class3.java
@@ -4,7 +4,9 @@ public class Class3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+System.out.println("this is sample project test");
+System.out.println("this is sample project test");
+System.out.println("this is sample project test");
 	}
 
 }
